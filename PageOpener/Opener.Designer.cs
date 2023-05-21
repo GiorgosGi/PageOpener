4855275e7573422337bfb5b3e10ec2b84642bfb4
INTTRUST\ggialouris <ggialouris@inttrust.gr>
2023-05-21 20:28:11 +0300

    Add project files.

---
+++ PageOpener/Opener.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace PageOpener
+{
+    partial class Opener
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.timeSpanNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.openButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.timeSpanNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlTextBox.Location = new System.Drawing.Point(12, 12);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(525, 23);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.TextChanged += new System.EventHandler(this.urlTextBox_TextChanged);
+            // 
+            // timeSpanNumericUpDown
+            // 
+            this.timeSpanNumericUpDown.Location = new System.Drawing.Point(12, 41);
+            this.timeSpanNumericUpDown.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.timeSpanNumericUpDown.Name = "timeSpanNumericUpDown";
+            this.timeSpanNumericUpDown.Size = new System.Drawing.Size(88, 23);
+            this.timeSpanNumericUpDown.TabIndex = 1;
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.SystemColors.Menu;
+            this.openButton.Location = new System.Drawing.Point(12, 70);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(88, 23);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "OpenButton";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Opener
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(549, 105);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.timeSpanNumericUpDown);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "Opener";
+            this.Text = "Opener";
+            this.Load += new System.EventHandler(this.Opener_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timeSpanNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox urlTextBox;
+        private NumericUpDown timeSpanNumericUpDown;
+        private Button openButton;
+    }
+}
---
+++ PageOpener/Opener.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PageOpener
+{
+    public partial class Opener : Form
+    {
+        public Opener()
+        {
+            InitializeComponent();
+        }
+
+        private string? currentUrl;
+        private bool loopUrl = true;
+        Process[] allProcesses = Process.GetProcesses();
+
+        private void OpenUrl(string url)
+        {
+            // Save the current URL
+            currentUrl = url;
+
+            // Open the URL in the default system browser
+            System.Diagnostics.Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void Wait(int seconds)
+        {
+            // Wait for the selected time span
+            System.Threading.Thread.Sleep(seconds * 1000);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the URL from the text box
+            string url = urlTextBox.Text;
+
+            while (loopUrl)
+            {
+                OpenUrl(url);
+                Wait((int)timeSpanNumericUpDown.Value);
+                foreach (var process in allProcesses)
+                {
+                    if (process.MainWindowTitle != "")
+                    {
+                        string s = process.ProcessName.ToLower();
+                        if (s == "chrome" || s == "firefox")      //needs a better way to close browser window
+                            process.Kill();
+                    }
+                }
+                System.Threading.Thread.Sleep(3000);
+            }
+        }
+
+        private void Opener_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void urlTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
